26209ee8c26e544919724eb5be2a02438eed6956
Xeninon <alecdscout@gmail.com>
2026-02-07 22:51:47 -0600

    llm boilerplate

---
+++ main.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key is None:
+    raise RuntimeError
+
+client = genai.Client(api_key=api_key)
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+# Now we can access 'args.user_prompt'
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+
+metadata = response.usage_metadata
+if metadata is None:
+    raise RuntimeError
+
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {metadata.prompt_token_count}")
+    print(f"Response tokens: {metadata.candidates_token_count}")
+
+print(response.text)
