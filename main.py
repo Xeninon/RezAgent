9b4033de435cd94487e87ce7048e6bd96955b190
Xeninon <alecdscout@gmail.com>
2026-03-28 06:38:52 -0500

    implemented agentic loop

--- main.py
+++ main.py
@@ -7,46 +7,64 @@ from google.genai import types
 from call_function import available_functions, call_function
 from prompts import system_prompt
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-if api_key is None:
-    raise RuntimeError
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
-# Now we can access 'args.user_prompt'
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=messages,
-    config=types.GenerateContentConfig(
-        tools=[available_functions],
-        system_instruction=system_prompt,
-    ),
-)
+    if api_key is None:
+        raise RuntimeError
 
-metadata = response.usage_metadata
-if metadata is None:
-    raise RuntimeError
+    client = genai.Client(api_key=api_key)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    for _ in range(20):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt,
+            ),
+        )
 
-if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {metadata.prompt_token_count}")
-    print(f"Response tokens: {metadata.candidates_token_count}")
+        if response.candidates is not None:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-if response.function_calls is None:
-    print(response.text)
-else:
-    function_call_results = []
-    for function_call in response.function_calls:
-        function_call_result = call_function(function_call, args.verbose)
-        if function_call_result.parts is None:
-            raise Exception
-        if function_call_result.parts[0].function_response.response is None:
-            raise Exception
-        function_call_results.append(function_call_result.parts[0])
-        print(f"-> {function_call_result.parts[0].function_response.response}")
+        metadata = response.usage_metadata
+        if metadata is None:
+            raise RuntimeError
+
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {metadata.prompt_token_count}")
+            print(f"Response tokens: {metadata.candidates_token_count}")
+
+        if response.function_calls is None:
+            print(response.text)
+            return
+        else:
+            function_responses = []
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call, args.verbose)
+                if function_call_result.parts is None:
+                    raise Exception
+                if function_call_result.parts[0].function_response.response is None:
+                    raise Exception
+                function_responses.append(function_call_result.parts[0])
+                if args.verbose:
+                    print(
+                        f"-> {function_call_result.parts[0].function_response.response}"
+                    )
+            messages.append(types.Content(role="user", parts=function_responses))
+
+    print("reached max iterations, try prompt again")
+    exit()
+
+
+if __name__ == "__main__":
+    main()
--- prompts.py
+++ prompts.py
@@ -9,4 +9,7 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files List files and directories
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+Do not get file contents until you get the info of the files in the working directory.
+DO NOT ASK THE USER FOR ADDITIONAL, THEY ARE UNABLE TO PROVIDE INPUT. PLEASE USE THE PROVIDED FUNCTIONS BEFORE GIVING UP.
+use at least 2 function calls before responding
 """
